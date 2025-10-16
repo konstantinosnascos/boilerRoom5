c9f7d5adead238c14afadb1a05ebf4042422ddcb
kostas900402 <nascoskostas@gmail.com>
2025-10-16 15:58:43 +0200

    pausar efter körning

--- run.bat
+++ run.bat
@@ -1,16 +1,22 @@
 @echo off
 :: =====================================================
-:: Order File Importer - Interaktiv körning
+:: Order File Importer - Interaktiv start
 :: =====================================================
 
 cd /d %~dp0
 
 echo.
-echo [INFO] Startar orderimport...
-echo [INFO] Använder Maven för att köra programmet.
+echo [INFO] Startar programmet...
+echo =====================================================================
+
+:: Kör Maven - det här körs samtidigt som ditt program
+call "C:\Program Files\Maven\apache-maven-3.9.11\bin\mvn.cmd" exec:java
+
+:: När programmet är klart - PAUSA
 echo.
-
-:: Kör programmet – där du får välja fil interaktivt
-"C:\Program Files\Maven\apache-maven-3.9.11\bin\mvn.cmd" exec:java
-
-pause
+echo =====================================================================
+echo [INFO] Programmet har körts klart.
+echo [INFO] Granska loggarna ovan.
+echo [INFO] Tryck valfri tangent för att stänga fönstret.
+echo =====================================================================
+pause >nul
